6a80c4e460902df1cbfd75e184eeb64b8cb71d93
andrew.chalmers <ajchalmers@gmail.com>
2022-06-09 11:36:24 +1000

    Allow add of gas measurements

---
+++ AcornSat.Visualiser/UiModel/DataSetLibraryEntry.cs
@@ -0,0 +1,12 @@
+﻿using static AcornSat.Core.Enums;
+
+namespace AcornSat.Visualiser.UiModel
+{
+    public class DataSetLibraryEntry
+    {
+        public string Name { get; set; }
+        public Guid SourceDataSetId { get; set; }
+        public DataAdjustment DataAdjustment { get; set; }
+        public DataType DataType { get; set; }
+    }
+}
