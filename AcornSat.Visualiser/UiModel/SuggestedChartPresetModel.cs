6e1ec72df6ff6dad1816ed48159717bb28f0fbc1
andrew.chalmers <ajchalmers@gmail.com>
2022-06-18 18:08:26 +1000

    Suggested charts

---
+++ AcornSat.Visualiser/UiModel/SuggestedChartPresetModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AcornSat.Visualiser.UiModel
+{
+    public class SuggestedChartPresetModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public List<ChartSeriesDefinition> ChartSeriesList { get; set; }
+    }
+
+    public class SuggestedChartPresetModelWithVariants : SuggestedChartPresetModel
+    {
+        public List<SuggestedChartPresetModel> Variants { get; set; }
+    }
+}
