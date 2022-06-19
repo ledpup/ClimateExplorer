e455ef7453e3683e5e94bc71a86e37e30f3bfb8e
andrew.chalmers <ajchalmers@gmail.com>
2022-06-19 20:48:27 +1000

    Label series editors with series colour. Avoid burning colours for moving averages

--- AcornSat.Visualiser/UiModel/ChartSeriesDefinition.cs
+++ AcornSat.Visualiser/UiModel/ChartSeriesDefinition.cs
@@ -95,6 +95,7 @@ namespace AcornSat.Visualiser.UiModel
         public int SmoothingWindow { get; set; }
         public SeriesAggregationOptions Aggregation { get; set; }
         public SeriesValueOptions Value { get; set; }
+        public string Colour { get; set; }
 
         // Rendering option fields
         public SeriesDisplayStyle DisplayStyle { get; set; }
