8f6740cfcf6eb264c0680ebd91ade5f4736aad41
Patrick Lea <ledpup@hotmail.com>
2022-11-04 15:33:19 +1100

    Blog integrated in + first post

--- ClimateExplorer.Visualiser/wwwroot/blog/assets/js/lunr/lunr-store.js
+++ ClimateExplorer.Visualiser/wwwroot/blog/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Welcome to the Climate Explorer blog",
         "excerpt":"Hey, welcome to the Climate Explorer blog! We plan to use this blog to keep you up to date as we add features &amp; data sets to Climate Explorer, as well as linking to further reading on the source data sets and climate concepts related to the site. We may...","categories": ["meta"],
         "tags": [],
-        "url": "/blog/meta/2022/06/27/welcome-to-jekyll.html",
+        "url": "/blog/meta/2022/11/04/welcome-to-climate-explorer.html",
         "teaser": null
       }]
