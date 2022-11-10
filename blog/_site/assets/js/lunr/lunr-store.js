1fcd00ddcc7ef512a64ba3bd77866705f7a50710
Patrick Lea <ledpup@hotmail.com>
2022-11-10 22:10:15 +1100

    Revert "Description text"
    This reverts commit ab9143def117baf796c027ba008fa584084d5a82.

--- blog/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome to the Climate Explorer blog",
-        "excerpt":"Hey, welcome to the Climate Explorer blog! We plan to use this blog to keep you up to date as we add features &amp; data sets to Climate Explorer, as well as linking to further reading on the source data sets and climate concepts related to the site. We may...","categories": ["meta"],
-        "tags": [],
-        "url": "/meta/2022/06/27/welcome-to-jekyll.html",
-        "teaser": null
-      }]
