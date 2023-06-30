93d3c9c040879485c0fd3ab9b08cb975c5062312
Patrick Lea <ledpup@hotmail.com>
2023-06-30 15:29:16 +1000

    Add new suggested chart

--- ClimateExplorer.Core/DataPreparation/Enums/SeriesDefinitionTypes.cs
+++ ClimateExplorer.Core/DataPreparation/Enums/SeriesDefinitionTypes.cs
@@ -4,5 +4,6 @@ public enum SeriesDerivationTypes
 {
     ReturnSingleSeries,
     DifferenceBetweenTwoSeries,
-    AverageOfMultipleSeries
+    AverageOfMultipleSeries,
+    //AverageAnomaliesOfMultipleSeries
 }
