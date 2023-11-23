07798961cb97116ff2f9dea22a2c10e26e2ca73d
Patrick Lea <ledpup@hotmail.com>
2023-11-23 17:34:29 +1100

    More blocked scope namespaces. Remove unnecessary usings

--- DPBlazorMapLibrary/Models/Layers/GridLayer.cs
+++ DPBlazorMapLibrary/Models/Layers/GridLayer.cs
@@ -1,15 +1,12 @@
-﻿using System.Drawing;
+﻿namespace DPBlazorMapLibrary;
 
-namespace DPBlazorMapLibrary
+/// <summary>
+/// Generic class for handling a tiled grid of HTML elements.
+/// This is the base class for all tile layers and replaces TileLayer.Canvas.
+/// GridLayer can be extended to create a tiled grid of HTML elements like <canvas>, <img> or <div>.
+/// GridLayer will handle creating and animating these DOM elements for you.
+/// </summary>
+public abstract class GridLayer : Layer
 {
-    /// <summary>
-    /// Generic class for handling a tiled grid of HTML elements.
-    /// This is the base class for all tile layers and replaces TileLayer.Canvas.
-    /// GridLayer can be extended to create a tiled grid of HTML elements like <canvas>, <img> or <div>.
-    /// GridLayer will handle creating and animating these DOM elements for you.
-    /// </summary>
-    public abstract class GridLayer : Layer
-    {
-       
-    }
+   
 }
--- DPBlazorMapLibrary/Models/Layers/GridLayerOptions.cs
+++ DPBlazorMapLibrary/Models/Layers/GridLayerOptions.cs
@@ -1,40 +1,37 @@
-﻿using System.Drawing;
+﻿namespace DPBlazorMapLibrary;
 
-namespace DPBlazorMapLibrary
+public class GridLayerOptions : LayerOptions
 {
-    public class GridLayerOptions : LayerOptions
+    public GridLayerOptions()
     {
-        public GridLayerOptions()
-        {
-            Pane = "tilePane";
-        }
-
-        /// <summary>
-        /// Width and height of tiles in the grid. Use a number if width and height are equal, or L.point(width, height) otherwise.
-        /// </summary>
-        public int TileSize { get; set; } = 256;
-
-        /// <summary>
-        /// Opacity of the tiles. Can be used in the createTile() function.
-        /// </summary>
-        public double Opacity { get; set; } = 1.0;
-
-        /// <summary>
-        /// By default, a smooth zoom animation will update grid layers every integer zoom level. Setting this option to false will update the grid layer only when the smooth animation ends.
-        /// </summary>
-        public bool UpdateWhenZooming { get; set; } = true;
-
-        /// <summary>
-        /// Tiles will not update more than once every updateInterval milliseconds when panning.
-        /// </summary>
-        public int UpdateInterval { get; set; } = 200;
-
-        public int ZIndex { get; set; } = 1;
-
-        /// <summary>
-        /// If set, tiles will only be loaded inside the set.
-        /// </summary>
-        public Tuple<LatLng, LatLng>? Bounds { get; set; }
-
+        Pane = "tilePane";
     }
+
+    /// <summary>
+    /// Width and height of tiles in the grid. Use a number if width and height are equal, or L.point(width, height) otherwise.
+    /// </summary>
+    public int TileSize { get; set; } = 256;
+
+    /// <summary>
+    /// Opacity of the tiles. Can be used in the createTile() function.
+    /// </summary>
+    public double Opacity { get; set; } = 1.0;
+
+    /// <summary>
+    /// By default, a smooth zoom animation will update grid layers every integer zoom level. Setting this option to false will update the grid layer only when the smooth animation ends.
+    /// </summary>
+    public bool UpdateWhenZooming { get; set; } = true;
+
+    /// <summary>
+    /// Tiles will not update more than once every updateInterval milliseconds when panning.
+    /// </summary>
+    public int UpdateInterval { get; set; } = 200;
+
+    public int ZIndex { get; set; } = 1;
+
+    /// <summary>
+    /// If set, tiles will only be loaded inside the set.
+    /// </summary>
+    public Tuple<LatLng, LatLng>? Bounds { get; set; }
+
 }
