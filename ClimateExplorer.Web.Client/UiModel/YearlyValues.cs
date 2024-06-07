7629df71c96e96b7f4a9b5aa075eadbbfdfc452e
Patrick Lea <ledpup@hotmail.com>
2024-06-07 20:02:27 +1000

    Emphasise change in value in climate stripe. Rainfall as percentage

--- ClimateExplorer.Web.Client/UiModel/YearlyValues.cs
+++ ClimateExplorer.Web.Client/UiModel/YearlyValues.cs
@@ -1,8 +1,9 @@
 ﻿namespace ClimateExplorer.Web.UiModel;
 
-public class YearlyValues(short year, double relative, double absolute)
+public class YearlyValues(short year, double relative, double absolute, double percentageOfAverage)
 {
     public short Year { get; set; } = year;
     public double Relative { get; set; } = relative;
     public double Absolute { get; set; } = absolute;
+    public double PercentageOfAverage { get; set; } = percentageOfAverage;
 }
