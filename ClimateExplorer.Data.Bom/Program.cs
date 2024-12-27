0949564e905bf51e0bf96b7ac939dac9865bde10
Patrick Lea <ledpup@hotmail.com>
2024-12-27 15:06:35 +1100

    Remove remote island and Antarctica references

--- ClimateExplorer.Data.Bom/Program.cs
+++ ClimateExplorer.Data.Bom/Program.cs
@@ -9,5 +9,3 @@ httpClient.DefaultRequestHeaders.AcceptLanguage.ParseAdd(acceptLanguage);
 var stations = await BomLocationsAndStationsMapper.BuildAcornSatLocationsFromReferenceMetaDataAsync(Guid.Parse("E5EEA4D6-5FD5-49AB-BF85-144A8921111E"), "_Australia_unadjusted");
 await BomDataDownloader.GetDataForEachStation(httpClient, stations);
 await BomLocationsAndStationsMapper.BuildAcornSatAdjustedDataFileMappingAsync(Guid.Parse("b13afcaf-cdbc-4267-9def-9629c8066321"), @"Output\DataFileMapping\DataFileMapping_Australia_unadjusted.json", "_Australia_adjusted");
-
-await BomLocationsAndStationsMapper.BuildRaiaLocationsFromReferenceMetaDataAsync(Guid.Parse("647b6a05-43e4-48e0-a43e-04ae81a74653"), "_Australia_Raia");
