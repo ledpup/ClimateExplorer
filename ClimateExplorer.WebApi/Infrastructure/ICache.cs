a91eb1daabf8aa926dd440e309170e80bca0b4c7
Patrick Lea <ledpup@hotmail.com>
2025-01-03 18:20:36 +1100

    Use the cache key if available when saving cache

--- ClimateExplorer.WebApi/Infrastructure/ICache.cs
+++ ClimateExplorer.WebApi/Infrastructure/ICache.cs
@@ -93,7 +93,14 @@ public class FileBackedTwoLayerCache : ICache
 
             if (mapping.TryGetValue(key, out var filename))
             {
-                var content = await File.ReadAllTextAsync(Path.Combine(cacheFolderName, filename));
+                var cacheFilePath = Path.Combine(cacheFolderName, filename);
+
+                if (!File.Exists(cacheFilePath))
+                {
+                    return default;
+                }
+
+                var content = await File.ReadAllTextAsync(cacheFilePath);
 
                 return JsonSerializer.Deserialize<T>(content);
             }
@@ -115,11 +122,14 @@ public class FileBackedTwoLayerCache : ICache
 
             var keyHash = GetDeterministicHashCode(key);
 
+            string filename = Guid.NewGuid().ToString() + ".json";
+
             // Check if the cache index contains the key
             var mapping = await ReadKeyToFileMappingForHashcode(keyHash);
-
-            Guid id = Guid.NewGuid();
-            string filename = Guid.NewGuid().ToString() + ".json";
+            if (mapping.ContainsKey(key))
+            {
+                filename = mapping[key];
+            }
 
             mapping[key] = filename;
 
