257b2a35f83c8ebe7e55707698e898f32526a6cf
Patrick Lea <ledpup@hotmail.com>
2026-04-01 23:18:22 +1100

    Codebehind

---
+++ ClimateExplorer.Web.Client/Components/Common/DelayedLoadingIndicator.razor.cs
@@ -0,0 +1,66 @@
+﻿namespace ClimateExplorer.Web.Client.Components.Common;
+
+using Microsoft.AspNetCore.Components;
+
+public partial class DelayedLoadingIndicator : IDisposable
+{
+    private bool showIndicator = false;
+
+    private bool delaying = false;
+
+    private CancellationTokenSource cts = new();
+
+    [Parameter]
+    public bool Visible { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> VisibleChanged { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public int ZIndex { get; set; }
+
+    public void Dispose()
+    {
+        cts.Cancel();
+        cts.Dispose();
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (Visible)
+        {
+            if (!delaying)
+            {
+                delaying = true;
+                _ = DelayIndicatorAsync();
+            }
+        }
+        else
+        {
+            showIndicator = false;
+        }
+    }
+
+    private async Task DelayIndicatorAsync()
+    {
+        try
+        {
+            await Task.Delay(500, cts.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            return;
+        }
+
+        delaying = false;
+        var currentIndicatorState = showIndicator;
+        showIndicator = Visible;
+        if (currentIndicatorState != showIndicator)
+        {
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
