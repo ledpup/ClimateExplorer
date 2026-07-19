fafc57f8dc4f74abfabf3e443ae9e340120582cb
Patrick Lea <ledpup@hotmail.com>
2026-07-19 18:01:34 +1000

    CO2 tooltip improvement

--- ClimateExplorer.Web.Client/Components/Co2/Co2NavTile.razor.cs
+++ ClimateExplorer.Web.Client/Components/Co2/Co2NavTile.razor.cs
@@ -1,5 +1,6 @@
 namespace ClimateExplorer.Web.Client.Components.Co2;
 
+using System.Globalization;
 using ClimateExplorer.Core.Model;
 using ClimateExplorer.WebApiClient.Services;
 using Microsoft.AspNetCore.Components;
@@ -12,7 +13,14 @@ public partial class Co2NavTile : IDisposable
 
     private CancellationTokenSource? cancellationTokenSource;
     private double? Value { get; set; }
+    private short? MeasurementYear { get; set; }
+    private short? MeasurementMonth { get; set; }
     private bool IsVisible { get; set; }
+
+    private string? TooltipText =>
+        Value.HasValue && MeasurementMonth.HasValue
+            ? $"<p>Atmospheric CO₂ (deseasonalised)</p><p>{Value.Value.ToString("F2")} parts per million</p><p>Recorded {CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(MeasurementMonth.Value)} {MeasurementYear}</p><p>From Mauna Loa Observatory</p>"
+            : null;
 
     [Inject]
     private IDataService DataService { get; set; } = default!;
@@ -33,22 +41,27 @@ public partial class Co2NavTile : IDisposable
         cancellationTokenSource = new CancellationTokenSource();
         var cancellationToken = cancellationTokenSource.Token;
 
+        ClimateRecordsResponse? response;
+
         try
         {
-            var response = await GetCo2(true);
+            response = await GetCo2(true);
 
             if (response is null)
             {
                 await Task.Delay(LoadDelayMs, cancellationToken);
                 response = await GetCo2(false);
-
-                Value = response!.Records.FirstOrDefault()?.Value;
             }
         }
         catch (OperationCanceledException)
         {
             return;
         }
+
+        var record = response?.Records.FirstOrDefault();
+        Value = record?.Value;
+        MeasurementYear = record?.Year;
+        MeasurementMonth = record?.Month;
 
         if (Value.HasValue)
         {
